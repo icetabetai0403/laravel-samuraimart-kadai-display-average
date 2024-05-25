763736936dbf4b6e214a45aca9a0171e2fc204a0
icetabetai0403 <spngbb.ptrc@gmail.com>
2024-05-25 18:57:35 +0900

    平均評価の表示を作成

---
+++ laravel-samuraimart-kadai-display-average/resources/views/web/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+  <div class="col-2">
+    @component('components.sidebar', ['categories' => $categories, 'major_categories' => $major_categories])
+    @endcomponent
+  </div>
+  <div class="col-9">
+    <h1>おすすめ商品</h1>
+    <div class="row">
+      @foreach ($recommend_products as $recommend_product)
+      <div class="col-4">
+        <a href="{{ route('products.show', $recommend_product) }}">
+          @if ($recommend_product->image !== "")
+          <img src="{{ asset($recommend_product->image) }}" class="img-thumbnail">
+          @else
+          <img src="{{ asset('img/dummy.png') }}" class="img-thumbnail">
+          @endif
+        </a>
+        <div class="row">
+          <div class="col-12">
+            <p class="samuraimart-product-label mt-2 mb-0">
+              {{ $recommend_product->name }}<br>
+              <div class="d-flex align-items-center">
+                <div class="star-rating d-flex" data-rate="{{ round($recommend_product->reviews->avg('score') * 2) / 2 }}"></div><div class="average-review-rate d-flex ms-1">{{ round($recommend_product->reviews->avg('score'), 1) }}</div>
+              </div>
+              <label>￥{{ $recommend_product->price }}</label>
+            </p>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+
+    <div class="d-flex justify-content-between">
+      <h1>新着商品</h1>
+      <a href="{{ route('products.index', ['sort' => 'id', 'direction' => 'desc']) }}">もっと見る</a>
+    </div>
+    <div class="row">
+      @foreach ($recently_products as $recently_product)
+      <div class="col-3">
+        <a href="{{ route('products.show', $recently_product) }}">
+          @if ($recently_product->image !== "")
+          <img src="{{ asset($recently_product->image) }}" class="img-thumbnail">
+          @else
+          <img src="{{ asset('img/dummy.png') }}" class="img-thumbnail">
+          @endif
+        </a>
+        <div class="row">
+          <div class="col-12">
+            <p class="samuraimart-product-label mt-2 mb-0">
+              {{ $recently_product->name }}<br>
+              <div class="d-flex align-items-center">
+                <div class="star-rating d-flex" data-rate="{{ round($recently_product->reviews->avg('score') * 2) / 2 }}"></div><div class="average-review-rate d-flex ms-1">{{ round($recently_product->reviews->avg('score'), 1) }}</div>
+              </div>
+              <label>￥{{ $recently_product->price }}</label>
+            </p>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </div>
+</div>
+@endsection
